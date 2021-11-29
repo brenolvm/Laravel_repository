311d7a796f356a84c192c761549fda2e373bda17
brenolvm <brenolvm@gmail.com>
2021-11-29 11:37:17 -0300

    Correção do migration userstable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTableSeeder extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -21,8 +21,6 @@ class CreateUsersTableSeeder extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-
-
         });
     }
 
